09e8523152a2dd7449566a1a735ae55124135295
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-04-28 14:32:37 +0300

    Detect owner label of requesting pod

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"regexp"
 	"time"
 
 	"k8s.io/api/core/v1"
@@ -56,11 +57,29 @@ func main() {
 	defer close(stop)
 	go reflector.Run(stop)
 
+	re := regexp.MustCompile(`^(.*):.*$`)
 	director := func(req *http.Request) {
-		klog.Infof("Got request: %+v", req)
-		klog.Infof("These are the pod IPs: %v", indexer.ListIndexFuncValues("ip"))
 		req.URL.Scheme = "http"
 		req.URL.Host = "127.0.0.1:9410"
+
+		klog.Infof("Got request: %+v", req)
+		klog.Infof("These are the pod IPs: %v", indexer.ListIndexFuncValues("ip"))
+		pods, err := indexer.ByIndex("ip", re.FindStringSubmatch(req.RemoteAddr)[1])
+		if err != nil {
+			klog.Error(err)
+			return
+		}
+		klog.Infof("It's from this pod(s): %+v", pods)
+		if len(pods) != 1 {
+			klog.Errorf("%+v does not have exactly one pod", pods)
+			return
+		}
+		podNew, ok := pods[0].(*v1.Pod)
+		if !ok {
+			klog.Errorf("%v is not a v1.Pod", pods)
+			return
+		}
+		klog.Infof("Owner: \"%s\"", podNew.ObjectMeta.Labels["owner"])
 	}
 	handler := &httputil.ReverseProxy{Director: director}
 	klog.Fatal(http.ListenAndServe(":9411", handler))
