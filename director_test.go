b764312ec84e603ed1aa0c9b9ed2f2d9851b511e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-05-01 17:37:36 +0300

    Add successful tag addition test

--- director_test.go
+++ director_test.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	. "github.com/onsi/gomega"
+	"github.com/tidwall/gjson"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	// testIp is the hardcoded value used by httptest.NewRequest in RemoteAddr
+	testIp = "192.0.2.1"
 )
 
 func TestProxyTargetURL(t *testing.T) {
@@ -15,4 +25,52 @@ func TestProxyTargetURL(t *testing.T) {
 	CreateDirector(CreateIndexer())(req)
 
 	g.Expect(req.URL.String()).To(Equal("http://127.0.0.1:9410" + path))
+}
+
+func TestOwnerTagAddition(t *testing.T) {
+	g := NewGomegaWithT(t)
+	owner := "from_label"
+
+	// Add requester pod to Indexer
+	indexer := CreateIndexer()
+	g.Expect(indexer.Add(&v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-pod",
+			Labels: map[string]string{
+				"owner": owner,
+			},
+		},
+		Status: v1.PodStatus{PodIP: testIp},
+	})).To(Succeed())
+	req := httptest.NewRequest("POST", "/api/v2/spans", strings.NewReader(`
+	[
+		{
+			"id": "352bff9a74ca9ad2",
+			"traceId": "5af7183fb1d4cf5f",
+			"parentId": "6b221d5bc9e6496c",
+			"name": "get /api",
+			"timestamp": 1556604172355737,
+			"duration": 1431,
+			"kind": "SERVER",
+			"localEndpoint": {
+				"serviceName": "backend",
+				"ipv4": "192.168.99.1",
+				"port": 3306
+			},
+			"remoteEndpoint": {
+				"ipv4": "172.19.0.2",
+				"port": 58648
+			},
+			"tags": {
+				"http.method": "GET",
+				"http.path": "/api"
+			}
+		}
+	]
+	`))
+	CreateDirector(indexer)(req)
+	body, err := ioutil.ReadAll(req.Body)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	g.Expect(gjson.GetBytes(body, "0.tags.owner").String()).To(Equal(owner))
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/onsi/gomega v1.7.0
+	github.com/tidwall/gjson v1.6.0
 	k8s.io/api v0.17.0
 	k8s.io/apimachinery v0.17.0
 	k8s.io/client-go v0.17.0
