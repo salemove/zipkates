f49a86c92566e82e5bf71685ef7d3464b7b546dd
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2020-05-05 20:07:51 +0300

    Finalize integration test
    Now it's something that exits with code 0 on success and code 1 on failure. In
    theory this makes it usable in a CI pipeline but in practice it hasn't been
    tested in a CI environment and I'm not sure how the k3d CLI would behave in
    one. Maybe it'd work in general because it should only really depend on Docker
    but I'm especially suspicious of the registry setup because it binds a port on
    the host that runs Docker which in CI hosts likely isn't the agent which itself
    is also running in Docker in Kubernetes.

--- test.sh
+++ test.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 
-k3d delete --keep-registry-volume || :
-k3d create --enable-registry --wait 300
+K3D_NAME='zipkin-test'
 
-KUBECONFIG="$(k3d get-kubeconfig --name='k3s-default')"
+# Start from a clean slate and ensure everything's cleaned up after the test
+function clean_up() {
+  echo "Removing k3d cluster if it exists .."
+  k3d delete --name="$K3D_NAME" || :
+}
+trap clean_up EXIT
+clean_up
+
+# Start the cluster and set up access
+k3d create --enable-registry --wait 300 --name="$K3D_NAME"
+KUBECONFIG="$(k3d get-kubeconfig --name="$K3D_NAME")"
 export KUBECONFIG
 
-docker build -t registry.local:5000/proxy:latest .
-docker push registry.local:5000/proxy:latest
+# Start a zipkin instance with the sidecar and wait for it to be ready
+docker build -t 127.0.0.1:5000/proxy:latest .
+docker push 127.0.0.1:5000/proxy:latest
 kubectl apply --wait=true -f setup.yml
 kubectl -n test-zipkin rollout status deploy/zipkin
+
+# Run the test
 kubectl apply --wait=true -f test.yml
+kubectl -n test-service wait --for=condition=complete --timeout=120s job/zipkin-client
